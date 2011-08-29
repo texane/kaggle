0207c728afc0f488f2256b98d72751780b427b09
le mentec fabien <fabien.lementec@imag.fr>
2011-08-29 09:31:47 -0500

    [add] main.cc

---
+++ cpc/src/main.cc
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <map>
+#include "table.hh"
+
+int main(int ac, char** av)
+{
+  table table;
+
+  if (table_create(table))
+  {
+    printf("[!] create\n");
+    return -1;
+  }
+
+  std::vector<table::col_type> col_types;
+
+#if 0 // fu.csv test
+
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::STRING);
+
+#else
+
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+  col_types.push_back(table::STRING);
+
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+
+  col_types.push_back(table::STRING);
+
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+  col_types.push_back(table::REAL);
+
+#endif
+
+  table_set_column_types(table, col_types);
+
+  if (table_read_csv_file(table, av[1], true))
+  {
+    printf("[!] read\n");
+    return -1;
+  }
+
+  // table_print(table);
+
+  table_destroy(table);
+
+  return 0;
+}
