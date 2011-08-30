12a833488f5bb5f187c0c034b83f583cd7df086a
Fabien Le Mentec <texane@gmail.com>
2011-08-30 02:42:24 -0500

    [add] gini.hh

---
+++ cpc/src/gini.hh
@@ -0,0 +1,11 @@
+#ifndef GINI_HH_INCLUDED
+# define GINI_HH_INCLUDED
+
+
+#include <vector>
+
+
+double GiniNormalized(const std::vector<double>&, const std::vector<double>&);
+
+
+#endif // ! GINI_HH_INCLUDED
