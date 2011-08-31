7bba12bc3381885e3cc9fb09abce40c8f5b51dbf
Fabien Le Mentec <texane@gmail.com>
2011-08-31 10:19:27 -0500

    [update] random forest tree use train_row_count

--- cpc/src/rf/main.cc
+++ cpc/src/rf/main.cc
@@ -14,25 +14,31 @@ int main(int ac, char** av)
 
   table table;
   table_create(table);
-  table_read_csv_file(table, av[1]);
+  table_read_csv_file(table, av[1], true);
+
+  // transform the claim amount
+  for (unsigned int i = 0; i < table.row_count; ++i)
+    table.rows[i][17] = ::floor(table.rows[i][17] / 10);
+
+  static unsigned int train_row_count = 10000;
 
   // turn into real_2d_array whose format is
   // var0, var1 .. varN, class
 
   alglib::real_2d_array x;
-  x.setlength(table.row_count, table.col_count);
-  for (unsigned int i = 0; i < table.row_count; ++i)
+  x.setlength(train_row_count, table.col_count);
+  for (unsigned int i = 0; i < train_row_count; ++i)
     for (unsigned int j = 0; j < table.col_count; ++j)
       x(i, j) = table.rows[i][j];
 
   // train a rdf
 
-  const alglib::ae_int_t npoints = table.row_count;
+  const alglib::ae_int_t npoints = train_row_count;
   const alglib::ae_int_t nvars = table.col_count - 1;
-  const alglib::ae_int_t nclasses = 2000;
+  const alglib::ae_int_t nclasses = 2000 / 10;
 
   const alglib::ae_int_t ntrees = 75; // recommended value
-  const double r = 0.3; // recommended value
+  const double r = 0.5; // recommended value
   alglib::ae_int_t info;
   alglib::decisionforest df;
   alglib::dfreport report;
@@ -40,13 +46,13 @@ int main(int ac, char** av)
   alglib::dfbuildrandomdecisionforest
     (x, npoints, nvars, nclasses, ntrees, r, info, df, report);
 
-  printf("o\n"); fflush(stdout);
-
   // build and classify the input set
   alglib::real_1d_array input;
-  for (unsigned int i = 0; i < table.row_count; ++i)
+  for (unsigned int i = train_row_count; i < table.row_count; ++i)
   {
-    input.setcontent(nvars, table.rows[i].data());
+    const table::row_type& row = table.rows[i];
+
+    input.setcontent(nvars, row.data());
 
     alglib::real_1d_array estims;
     estims.setlength(nclasses);
@@ -59,7 +65,7 @@ int main(int ac, char** av)
       if (estims[j] > estims[k]) k = j;
 
     // print the class
-    printf("%lf %u\n", table.rows[i][17], k);
+    printf("%lf %u\n", row[row.size() - 1], k);
   }
 
   table_destroy(table);
