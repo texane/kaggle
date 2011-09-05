409c0907ee158aaa54a770a37288618e874d3599
Fabien Le Mentec <texane@gmail.com>
2011-09-05 07:04:15 -0500

    [update] wip

--- cpc/src/randomjungle/predict.sh
+++ cpc/src/randomjungle/predict.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 ./a.out \
 --predict=o.jungle.xml \
---file=/tmp/fu.csv \
+--file=$HOME/repo/kaggle/cpc/data/test_set_reals.csv \
 --delimiter=, \
 --varnamesrow=0 \
 --colselection=predict.cols \
--- cpc/src/randomjungle/train.sh
+++ cpc/src/randomjungle/train.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env sh
 ./a.out \
---file=$HOME/repo/kaggle/cpc/data/fu.csv \
+--file=$HOME/repo/kaggle/cpc/data/all_valid_nonzero.csv \
 --delimiter=, \
---ntree=100 \
+--ntree=75 \
 --impmeasure=1 \
 --varnamesrow=0 \
 --colselection=train.cols \
---nrow=5000 \
 --votes \
 --verbose \
 --outprefix=o \
 --depvarname=Claim_Amount \
 --summary \
 --write=2
+
+# --nrow=5000
